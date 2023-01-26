072bb5c105c7bbc694a935f4f2c8c44671b40ed1
David Roulet <david.roulet@cpnv.ch>
2023-01-26 14:40:07 +0100

    wip

--- api-client/socket_client.py
+++ api-client/socket_client.py
@@ -2,11 +2,11 @@ import socket
 
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
+    host = "10.12.0.3"  # as both code is running on same pc
+    port = 5001  # socket server port number
 
     client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket.connect((host, 5001))  # connect to the server
 
     message = input(" -> ")  # take input
 
